4c4a3d017e22f6a171941afc7e2b6700385a54d9
Brendon <brendonjames@Annikas-MacBook-Pro.local>
2022-02-03 14:16:22 -0800

    fixed a minor bug

--- authenticate-me/backend/Routes/index.js
+++ authenticate-me/backend/Routes/index.js
@@ -5,10 +5,13 @@ const apiRouter = require('./api/index');
 
 router.use('/api', apiRouter);
 
-router.get('/hello/world', function (req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
+
 
 
 module.exports = router;
+
+// //ROUTE FOR TESTING IN BROWSWER DURING SETUP
+// router.get('/hello/world', function (req, res) {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     res.send('Hello World!');
+// });
