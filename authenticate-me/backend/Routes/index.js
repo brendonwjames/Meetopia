7537d2ce4429cdcfa724789a8439ff93123fed16
Brendon <brendonjames@Annikas-MacBook-Pro.local>
2022-02-03 10:49:28 -0800

    added error handling

--- authenticate-me/backend/Routes/index.js
+++ authenticate-me/backend/Routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api/index');
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
+router.use('/api', apiRouter);
+
+router.get('/hello/world', function (req, res) {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.send('Hello World!');
 });
+
 
 module.exports = router;
