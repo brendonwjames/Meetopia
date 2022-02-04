c7bf573534baf61ea4026ac984e67f585f70e051
Brendon <brendonjames@Annikas-MacBook-Pro.local>
2022-02-04 15:15:42 -0800

    made login form page a modal

--- authenticate-me/frontend/src/App.js
+++ authenticate-me/frontend/src/App.js
@@ -1,13 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { Route, Switch } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage';
-import SignupFormPage from './components/SignupFormPage';
-import * as sessionActions from './store/session';
-import Navigation from './components/Navigation';
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import SignupFormPage from "./components/SignupFormPage";
+import * as sessionActions from "./store/session";
+import Navigation from "./components/Navigation";
 
 function App() {
-
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
@@ -19,10 +17,7 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
-          <Route path='/signup'>
+          <Route path="/signup">
             <SignupFormPage />
           </Route>
         </Switch>
