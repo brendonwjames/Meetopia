6870ad4b32ffdcfe854046362bc7e0f8bd8d3398
Brendon <brendonjames@Annikas-MacBook-Pro.local>
2022-02-03 20:43:11 -0800

    started session actions and reducer

---
+++ authenticate-me/frontend/src/store/session.js
@@ -0,0 +1,55 @@
+import { csrfFetch } from './csrf';
+
+//POPJO action creators (the variables used in the reducer)
+const ADD_USER = 'session/addUser';
+const REMOVE_USER = 'session/removeUser';
+
+//the actual action creators
+const addUser = (user) => {
+    return {
+        type: ADD_USER,
+        user
+    };
+};
+
+const removeUser = () => {
+    return {
+        type: REMOVE_USER
+    };
+};
+
+//login thunk
+//call the API to login, set session user from the response
+export const login = (user) => async (dispatch) => {
+    const { credential, password } = user;
+    const response = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify({
+            credential,
+            password
+        }),
+    });
+    const data = await response.json();
+    dispatch(addUser(data.user));
+    return response;
+}
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case ADD_USER:
+            newState = Object.assign({}, state);
+            newState.user = action.user;
+            return newState;
+        case REMOVE_USER:
+            newState = Object.assign({}, state);
+            newState.user = null;
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default sessionReducer;
