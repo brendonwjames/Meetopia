591cbf7847ebc4b4b6dc5943d74793aa93997f67
Brendon <brendonjames@Annikas-MacBook-Pro.local>
2022-02-13 20:40:26 -0800

    added favicon

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -6,7 +6,7 @@ function SplashPage() {
     return (
         <div className='splash-page-container'>
             <h1 className='splash-header'>Welcome to Meetopia!</h1>
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR29bZAB7x179VCSyTf4TnDeaqJk0v6dz1DKA&usqp=CAU' alt='https://cdn-60c35131c1ac185aa47dd21e.closte.com//wp-content/uploads/2021/01/file-cant-found.png'></img>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7BN2sOUwTHB2gKhTrx3qyQwDEn_9-7RXJCU6pr9_7vq_Eg-WbLGMJsp2hUBbITjf3168&usqp=CAU' alt='https://cdn-60c35131c1ac185aa47dd21e.closte.com//wp-content/uploads/2021/01/file-cant-found.png'></img>
         </div>
     );
 }
